21342799d2717ca8b105005aeb1ad44a09ca90dd
Hesham el sahhar <hesham.elsahhar@gmail.com>
2018-03-10 21:55:20 +0200

    adjusting opening two cards and rotation

--- app.js
+++ app.js
@@ -1,11 +1,53 @@
-function openCard(ev)
+let isopen=false;
+let opened;
+let curropen=0;
+function openCard(ev,arr)
 {
+if (curropen==2)return 0;
     const givencard=ev.target;
+    ev.stopPropagation();
+    console.log("in");
     givencard.classList.add("show");
-    console.log(givencard);
+    setTimeout(()=>{givencard.classList.add("now");}, 200);
+    if (isopen==false){
+        opened=ev.target;
+        isopen=true;
+        curropen=1;
+    }
+    else 
+    {
+    curropen=2;
+        setTimeout(() => {
+            if (givencard.firstElementChild.getAttribute("src")!==opened.firstElementChild.getAttribute("src"))
+            {
+            givencard.classList.remove("show","now");
+            opened.classList.remove("show","now");
+            console.log("Not equal");
+            curropen=0;
+            }
+        isopen=false;
+    }, 2000);
+    
+    }
 
 }
-const elem = document.getElementsByClassName("card");
-for (let i=0;i<elem.length;i++){
-elem[i].addEventListener("click",function(){openCard(event)});
+function shuffle (x)
+{
+
+    for (let i=arr.length-1;i>=0;i--)
+    {
+        let j=Math.floor(Math.random()*(i+1));
+        [x[i],x[j]]=[x[j],x[i]];
+    }
 }
+    const elem = document.getElementsByClassName("card");
+    let arr=Array.apply(null, {length: 8}).map(Number.call,Number);
+    arr=arr.concat(Array.apply(null, {length: 8}).map(Number.call,Number));
+    shuffle(arr);
+    for (let i=0;i<elem.length;i++)
+    {
+        elem[i].addEventListener("click",function(){openCard(event,elem)});
+        let child = document.createElement("img");
+        child.setAttribute("src",`img/${arr[i]+1}.png`);
+        elem[i].appendChild(child);
+    }
